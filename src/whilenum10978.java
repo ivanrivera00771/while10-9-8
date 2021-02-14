5b980f8e9b1c6920d94952970e91fccc3ae83100
IvanRivera771 <47260927+IvanRivera771@users.noreply.github.com>
2021-02-14 10:01:14 -0600

    while desencendente

---
+++ src/whilenum10978.java
@@ -0,0 +1,15 @@
+
+public class whilenum10978 {
+    
+    public static void main(String[] agrs){
+    
+    //BUCLE WHILE
+        int i = 10;
+
+        while (i>=1) {
+            System.out.println(i);
+            i--; //== i=i+1;
+
+        }
+}
+}
